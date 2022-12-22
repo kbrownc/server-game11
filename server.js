0cc9f69a2569feb1749b51f2b6c569a0620631c4
Kim Brown <kbrownc341@gmail.com>
2022-12-22 15:29:45 -0500

    Added to receivegame

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ io.on('connection', socket => {
 	const id = socket.handshake.query.id
 	socket.join(id)
 	socket.on('send-message', ({ recipients, text }) => {
+		console.log('send-message',recipients,text)
 		if (recipients !== undefined) recipients.forEach(recipient => {
 			const newRecipients = recipients.filter(r => r !== recipient)
 			newRecipients.push(id)
@@ -17,12 +18,11 @@ io.on('connection', socket => {
 		})
 	})
 	socket.on('send-game', ({ recipients, text }) => {
-		if (recipients !== undefined) recipients.forEach(recipient => {
-			const newRecipients = recipients.filter(r => r !== recipient)
-			newRecipients.push(id)
-			socket.broadcast.to(recipient).emit('receive-game', {
-				recipients: newRecipients, sender: id, text
+		console.log('send-game',recipients, id, text)
+		if (recipients !== undefined) {
+			socket.broadcast.to(recipients).emit('receive-game', {
+				sender: id, text
 			})
-		})
+		}
 	})
 })
